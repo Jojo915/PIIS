2a08b0a1d1670f417f79773fd6f7f59dd067e233
amndzdzdz <zorrofx@gmx.net>
2026-05-16 10:44:57 +0200

    feat: added inital project structure

---
+++ backend/README.md
@@ -0,0 +1,1 @@
+## Backend for the PIIS, contains the AI stuff
---
+++ backend/tests/test_main.py
@@ -0,0 +1,10 @@
+"""Main file."""
+
+
+def main():
+    """Call main function."""
+    print("Hello from backend!")
+
+
+if __name__ == "__main__":
+    main()
