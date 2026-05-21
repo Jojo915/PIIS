361de817d6caa098b5412f02efe5029929ba157d
amndzdzdz <zorrofx@gmx.net>
2026-05-21 19:27:04 +0200

    feat: added LLM code

---
+++ backend/src/app/inference/client.py
@@ -0,0 +1,12 @@
+"""Contains the client to run inference."""
+
+from __future__ import annotations
+
+from openai import OpenAI
+
+
+def get_client(
+    api_key: str = "EMPTY", api_url: str = "http://localhost:8000/v1"
+):
+    """Construct and return the OpenAI API client."""
+    return OpenAI(api_key=api_key, base_url=api_url, timeout=300.0)
